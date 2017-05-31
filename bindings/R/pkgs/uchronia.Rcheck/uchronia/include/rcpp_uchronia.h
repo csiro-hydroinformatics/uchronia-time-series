1d2043a13180c789e128a48e239a81e1ea0bb86a
J-M <jean-michel.perraud@csiro.au>
2017-05-31 15:40:50 +1000

    Revert "Adding uchronia R package header files"
    This reverts commit 6a893e02e51abaac2b9c9796094a973e65003aff.

--- bindings/R/pkgs/uchronia.Rcheck/uchronia/include/rcpp_uchronia.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <Rcpp.h>
-#include "uchronia_wrapper_setup.h"
-#include "cinterop/rcpp_interop.hpp"
-
--- bindings/R/pkgs/uchronia.Rcheck/uchronia/include/uchronia_wrapper_setup.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "uchronia_r_exports.h"
-#include "moirai/opaque_pointers.hpp"
-using moirai::opaque_pointer_handle;
