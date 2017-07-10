3f3dc73993fcdea17bbf684070eb923a2e36d8ec
J-M <jean-michel.perraud@csiro.au>
2017-07-10 16:51:03 +1000

    start to move to capigen for glue code generation

--- bindings/create_r_uchronia.r
+++ bindings/create_r_uchronia.r
@@ -1,8 +1,8 @@
 
-library(swiftdev)
+library(capigen)
 load_wrapper_gen_lib('c:/src/github_jm/rcpp-wrapper-generation')
 uchroniaSrcPath <- 'c:/src/csiro/stash/datatypes'
-generate_uchronia_r_all_wrappers(uchroniaSrcPath)
-roxy_uchronia()
+# generate_uchronia_r_all_wrappers(uchroniaSrcPath)
+# roxy_uchronia()
 
-# devmode() ; prepend_rlibpath() ; library(uchronia)
+generate_uchronia_python_all_wrappers(uchroniaSrcPath)
