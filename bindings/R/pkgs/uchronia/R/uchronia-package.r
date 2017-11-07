bffb83ad93c32286da53266de6b08b571fcc0512
J-M <jean-michel.perraud@csiro.au>
2017-11-07 13:47:28 +1100

    bump r pkg version nb

--- bindings/R/pkgs/uchronia/R/uchronia-package.r
+++ bindings/R/pkgs/uchronia/R/uchronia-package.r
@@ -5,17 +5,18 @@
 #' \tabular{ll}{
 #' Package: \tab uchronia \cr
 #' Type: \tab Package\cr
-#' Version: \tab 0.3.2 \cr
-#' Date: \tab 2017-08-09 \cr
-#' Release Notes: \tab Added features needed for RPP for interop from/to R. Enhanced some of the C API. \cr
+#' Version: \tab 0.3.3 \cr
+#' Date: \tab 2017-11-07 \cr
+#' Release Notes: \tab Add feature to transform each item in an ensemble forecast time series, with aggregation/disaggregation for now. Normal maintenance and tidy ups \cr
 #' License: \tab LGPL-3 \cr
 #' }
 #'
 #' \tabular{lll}{
 #' Version \tab Date \tab Notes \cr
-#' 0.3.1 \tab 2017-07-21 Added features needed for RPP. Enhanced some of the C API. \cr
-#' 0.2.2 \tab 2017-06-22 Minor functions added. Note that the R package itself (i.e. not the C++ library) is still new and much documentation remains to be written/ported from other packages. \cr
-#' 0.2.1 \tab 2017-05-26 First version with port of all features from SWIFT, and succinct vignette \cr
+#' 0.3.2 \tab 2017-08-09 \tab Added features needed for RPP for interop from/to R. Enhanced some of the C API \cr
+#' 0.3.1 \tab 2017-07-21 \tab Added features needed for RPP. Enhanced some of the C API. \cr
+#' 0.2.2 \tab 2017-06-22 \tab Minor functions added. Note that the R package itself (i.e. not the C++ library) is still new and much documentation remains to be written/ported from other packages. \cr
+#' 0.2.1 \tab 2017-05-26 \tab First version with port of all features from SWIFT, and succinct vignette \cr
 #' }
 #'
 #' @import xts
