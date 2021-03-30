e3e8cd4497cad50cebe6e4225f4735b17c2740f7
J-M <jean-michel.perraud@csiro.au>
2021-03-30 19:29:02 +1100

    Update setup.py

--- bindings/python/uchronia/setup.py
+++ bindings/python/uchronia/setup.py
@@ -8,101 +8,65 @@ https://bitbucket.csiro.au/projects/SF/repos/datatypes/browse
 from setuptools import setup, find_packages
 # To use a consistent encoding
 from codecs import open
-from os import path
+import os
+import re
 
-here = path.abspath(path.dirname(__file__))
+here = os.path.abspath(os.path.dirname(__file__))
+
+verstr = 'unknown'
+VERSIONFILE = "uchronia/_version.py"
+with open(VERSIONFILE, "r")as f:
+    verstrline = f.read().strip()
+    pattern = re.compile(r"__version__ = ['\"](.*)['\"]")
+    mo = pattern.search(verstrline)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+# with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+#     long_description = f.read()
+
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
+    long_description_content_type='text/markdown'
+
+REQUIREMENTS = ['refcount', 'cffi', 'cinterop']
+
+TEST_REQUIREMENTS = ['pytest',
+                    #  'coveralls',
+                    #  'pytest-cov',
+                    #  'pytest-mpl'
+                     ]
+
+CLASSIFIERS = ['Development Status :: 3 - Alpha',
+                'Intended Audience :: Developers',
+                'Topic :: Software Development ',
+                # 'License :: OSI Approved :: MIT License',
+                'Operating System :: OS Independent',
+                'Programming Language :: Python'
+                ]
 
 setup(
     name='uchronia',
-
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.6.1',
-
-    description='Bindings for the C++ uchronia library',
+    version=verstr,
+    description='Uchronia - time series handling for ensembles simulations and forecasts in C++', 
     long_description=long_description,
-
-    # The project's main homepage.
-    url='https://bitbucket.csiro.au/projects/SF/repos/datatypes/browse',
-
-    # Author details
+    long_description_content_type=long_description_content_type,
+    url='https://github.com/csiro-hydroinformatics/uchronia-time-series',
     author='Jean-Michel Perraud',
-    author_email='jean-michel.perraud@csiro.au',
-
-    # Choose your license
-    license='AGPL-3',
-
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-
-        # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: AGPL-3',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
-    ],
-
-    # What does your project relate to?
-    keywords='''datatypes "time series" ''',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    #   py_modules=["my_module"],
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[],
-
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
-    extras_require={
-        # 'dev': ['check-manifest'],
-        # 'test': ['coverage'],
+    author_email='per202@csiro.au',
+    classifiers=CLASSIFIERS,
+    keywords='ensemble forecast time series',
+    packages=['uchronia', 'uchronia.wrap'],
+    install_requires=REQUIREMENTS,
+    project_urls={  # Optional
+        'Bug Reports': 'https://github.com/csiro-hydroinformatics/uchronia-time-series/issues',
+        # 'Funding': 'https://donate.pypi.org',
+        # 'Say Thanks!': 'http://saythanks.io/to/example',
+        'Source': 'https://github.com/csiro-hydroinformatics/uchronia-time-series',
     },
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    package_data={
-        # 'sample': ['package_data.dat'],
-    },
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    # data_files=[('my_data', ['data/data_file'])],
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
-    entry_points={
-        'console_scripts': [
-            # 'sample=sample:main',
-        ],
-    },
+    include_package_data=True, # C definitions for CFFI 
+    package_data={'': ['data/*.h']},
 )
