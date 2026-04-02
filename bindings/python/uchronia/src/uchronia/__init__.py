81eda0d1c8689accd9510d095424cd238eb87087
J-M <jean-michel.perraud@csiro.au>
2026-04-02 17:24:50 +1100

    refactor: WIP port to new package structure

--- bindings/python/uchronia/src/uchronia/__init__.py
+++ bindings/python/uchronia/src/uchronia/__init__.py
@@ -5,6 +5,18 @@ Python interface to a C++ library designed to handle multidimensional time serie
 
 from __future__ import annotations
 
+# Trigger the loading of dependency package first
+# May guard against obscure bugs. Bug not observed in uchronia or swift, but definitely for fogss package. 
+# upcast of opaque external pointers (e.g. passing a COMPOSITE_PARAMETERIZER_PTR to an API
+# function that accepts HYPERCUBE_PTR)
+# Candidate fix for https://jira.csiro.au/projects/WIRADA/issues/WIRADA-640 is thus:
+import cinterop as __cinterop
+
+# Trigger the initialisation of the custom wrapper generation function.
+import uchronia.classes as __uclasses
+
+# Trigger the loading of the native library.
+import uchronia.wrap.ffi_interop as __interop
 from uchronia._internal.cli import get_parser, main
 
 __all__: list[str] = ["get_parser", "main"]
